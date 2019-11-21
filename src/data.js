3371226e56ae8453c74c55e25b863e47d2043d36
Rajani Dulal <dulalrajani77@gmail.com>
2019-11-21 14:20:17 -0600

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,172 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Ethics in Information Technology",
+    img: "img/product-10.png",
+    price: 24,
+    company: "George W. Reynolds",
+    info:
+      "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here! ",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+ ,
+  {
+    id: 2,
+    title: "The C++ Programming Language",
+    img: "img/product-7.png",
+    price: 300,
+    company: "Bjarne Stroustrup",
+    info:
+      "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here!",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+
+  {
+    id: 3,
+    title: "Hacking for Dummies",
+    img: "img/product-4.png",
+    price: 180,
+    company: "Kevin Beaver",
+    info:
+      "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here!",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  
+ 
+  {
+    id: 4,
+    title: "CompTIA Security+ Certification Guide",
+    img: "img/product-6.png",
+    price: 270,
+    company: "Ian Nell",
+    info:
+      "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here!",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "The Complete Software Developer's Career Guide I",
+    img: "img/product-5.png",
+    price: 250,
+    company: "John Sonmez",
+    info:
+      "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here! ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+
+  {
+    id: 6,
+    title: "The Complete Software Developer's Career Guide II",
+    img: "img/product-5.png",
+    price: 260,
+    company: "John Sonmez",
+    info:
+      "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here! ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "The Complete Software Developer's Career Guide III",
+    img: "img/product-5.png",
+    price: 275,
+    company: "John Sonmez",
+    info:
+      "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here! ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "The Complete Software Developer's Career Guide IV",
+    img: "img/product-5.png",
+    price: 300,
+    company: "John Sonmez",
+    info:
+      "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here! ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 9,
+    title: "Cisco CCNA Networking For Beginners",
+    img: "img/product-1.png",
+    price: 200,
+    company: "Ron Gilster",
+    info:
+    "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here! ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 10,
+    title: "Building a HIPAA-Compliant Cybersecurity Program",
+    img: "img/product-8.png",
+    price: 350,
+    company: "Eric C. Thompson",
+    info:
+      "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here!",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 11,
+    title: "CyberSecurity",
+    img: "img/product-3.png",
+    price: 350,
+    company: "Lester Evans",
+    info:
+      "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here! ",
+      inCart: false,
+    count: 0,
+    total: 0
+  } ,
+  
+ 
+  {
+    id: 12,
+    title: "CCNA Routing and Switching",
+    img: "img/product-2.png",
+    price: 180,
+    company: "Allan Johnson",
+    info:
+      "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here! ",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+ 
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Welcome to hub of books",
+  img: "img/product-1.png",
+  price: 10,
+  company: "Book Publication",
+  info:
+    "Hello everyone. Welcome to my pages. You can advertise your books here. Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here! Include description here!Include description here! Include description here!",
+  inCart: false,
+  count: 0,
+  total: 0
+};
